1fa6f47a483befb677bdeeac150130f08adbbed0
Thien K. M. Bui <kevinbui904@gmail.com>
2022-01-22 18:17:04 -0600

    added search

--- exam1-bui/main.scm
+++ exam1-bui/main.scm
@@ -9,7 +9,26 @@
 ;return: flat list with all items and subitems in lst being reversed
 (define reverse-reverse 
     (lambda (lst)
-        (if (null? lst) 
+        (reverse-reverse-helper (reverse lst))))
+
+;reverse-reverse-helper
+;param: list of lists
+;return: will reverse ALL sublists and turn them into 1 flat list 
+(define reverse-reverse-helper
+    (lambda (lst)
+            (if (null? lst) 
             '()
-            
-            )))
+            (append (reverse (car lst)) (reverse-reverse-helper (cdr lst))))))
+
+;search
+;param: list of lists lst, value val
+;return: true if the value is found in the list of lists, otherwise false
+(define search 
+    (lambda (lst val)
+        (if (and (not (null? lst)) (list? lst))
+            (or (search (car lst) val) (search (cdr lst) val))
+            (if (equal? lst val)
+                #t 
+                #f)
+                )))
+
