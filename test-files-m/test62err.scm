e2facc4d06d50a617cc05c1c9578e13e1664dada
Thien K. M. Bui <kevinbui904@gmail.com>
2022-03-13 14:13:56 -0500

    merged all test cases, they all pass

--- test-files-m/test62err.scm
+++
@@ -1,3 +0,0 @@
-(let ((x 3))
-  (letrec ((y x) (x 4))
-    y))
--- test-files-m/test73.scm
+++
@@ -1,4 +0,0 @@
-; Note: this is testing whether you've implemented
-; and properly as a special form! (You don't need
-; division implemented to pass this test.)
-(and #f (if (= (/ 2 0) 3) #f #t))
